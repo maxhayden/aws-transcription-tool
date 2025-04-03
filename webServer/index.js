10b2c1268b704ad81e27ab98738efef547268a24
Maxwell Hayden <mx337324@dal.ca>
2025-04-03 11:52:02 -0300

    coverted to new front

---
+++ webServer/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
+
+// Set EJS View Engine**
+app.set('view engine','ejs');
+// Set HTML engine**
+app.engine('html', require('ejs').renderFile);
+//set directory
+app.set('views', __dirname + '/views');
+//static folder
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    //open form.html from the views directory
+    res.render('index');
+});
+
+app.get('/home', function(req, res) {
+    //open form.html from the views directory
+    res.render('index');
+});
+
+app.get('/dashboard', function(req, res) {
+    //open form.html from the views directory
+    res.render('app');
+});
+
+app.post('/', urlencodedParser, function(req, res) {
+    //retrieve first and lastname
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    //open submitted.html after the user has submitted the form
+    res.render('submitted', {output: req.body.firstName});
+});
+
+app.listen(3000);
