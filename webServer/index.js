8da768ff7ec73496f61dc301957360860e64a66d
Maxwell Hayden <mx337324@dal.ca>
2025-04-07 20:48:56 -0300

    now doing photos

--- webServer/index.js
+++ webServer/index.js
@@ -7,32 +7,32 @@ console.log("SECRET_KEY:", process.env.SECRET_KEY);
 console.log("REGION:", process.env.REGION);
 
 const multer = require('multer');
-const {memoryStorage} = require('multer');
+const { memoryStorage } = require('multer');
 const storage = memoryStorage();
-const upload = multer({storage});
+const upload = multer({ storage });
 
 
 const app = express();
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     //home page
     res.render('index');
 });
 
-app.get('/home', function(req, res) {
+app.get('/home', function (req, res) {
     //home page
     res.render('index');
 });
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
     //login page
     res.render('login');
     //write all the functionality to check if a user exists and then log them in
@@ -42,18 +42,18 @@ app.get('/login', function(req, res) {
     //get the user input
     const username = req.query.username;
     const password = req.query.password;
-    
+
     //check if username exists in the database
 
 
     //check if the username password is right
-    if (password == the_actual_password){
+    if (password == the_actual_password) {
         res.write("You are logged in");
     }
 });
 
 
-app.get('/dashboard', function(req, res) {
+app.get('/dashboard', function (req, res) {
     //open form.html from the views directory
     res.render('app');
 });
@@ -63,19 +63,19 @@ app.get('/dashboard', function(req, res) {
 const s3 = new S3Client({
     region: process.env.REGION,
     credentials: {
-        accessKeyId:process.env.ACCESS_KEY,
+        accessKeyId: process.env.ACCESS_KEY,
         secretAccessKey: process.env.SECRET_KEY,
         sessionToken: process.env.SESSION_TOKEN
-    } 
+    }
 })
 
-const uploadAudio = async (fileName, bucketName, file) => {
+const uploadImage = async (fileName, bucketName, file) => {
 
     const params = {
         Bucket: bucketName,
         Key: fileName,
         Body: file,
-        ContentType: 'audio/mpeg',
+        ContentType: 'image/png',
     }
 
 
@@ -83,13 +83,16 @@ const uploadAudio = async (fileName, bucketName, file) => {
 }
 
 //post to s3 bucket
-app.post('/dashboard/process', upload.single('audiofile'), async (req, res) =>{
+app.post('/dashboard', upload.single('image'), async (req, res) => {
 
-    const fileName = 'my first upload';
+    console.log("BODY:", req.body);
+    console.log("FILE:", req.file);
+
+    const fileName = req.body.fileName;
     const bucketName = process.env.S3_INPUT;
     const file = req.file.buffer;
     try {
-        const result = await uploadAudio(fileName, bucketName, file);
+        const result = await uploadImage(fileName, bucketName, file);
         console.log(result);
         res.send("Uploaded successfully");
     } catch (err) {
@@ -98,9 +101,11 @@ app.post('/dashboard/process', upload.single('audiofile'), async (req, res) =>{
     }
 });
 
-app.use(function(req,res,next){ 
-    res.status(404).render('404'); 
-}); 
+app.use(function (req, res, next) {
+    res.status(404).render('404');
+});
 
-console.log("listening on port 3000");
-app.listen(80);
+
+const port = 80;
+console.log(`listening on port ${port}`);
+app.listen(port);
